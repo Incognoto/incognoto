fbf3c98b0db8a078021f7605cd2a8df48e042ad8
leroosje <leroosje@uncg.edu>
2018-03-29 23:19:17 -0700

    Hooked up NoteSearch to NoteManager and changed accordingly

--- android/SumNotes/src/main/java/com/notes/sum/sec/NoteManager.java
+++ android/SumNotes/src/main/java/com/notes/sum/sec/NoteManager.java
@@ -299,20 +299,20 @@ public class NoteManager {
 
     // Given a user defined query return all Note objects that contain terms related to the query
     public static void search(String query) {
-//        ArrayAdapter<Note> tempAdapter = adapter;
-//        NoteSearch searchManager = new NoteSearch(adapter, query);
-//        filteredAdapter = searchManager.getFoundNoteList();
-//        List<Note> found = new ArrayList<>();
+        ArrayAdapter<Note> tempAdapter = adapter;
+        NoteSearch searchManager = new NoteSearch(adapter, query);
+        filteredAdapter = searchManager.getFoundNoteList();
+        List<Note> found = new ArrayList<>();
 
-        for (int i = 0; i < adapter.getCount(); i++) {
-            Note note = adapter.getItem(i);
-            String content = note.getNoteContent();
-
-            // TODO: This currently looks for exact string matches. It should split the query into words and search for each one
-            if (content.contains(query)) {
-                filteredAdapter.add(note);
-            }
-        }
+//        for (int i = 0; i < adapter.getCount(); i++) {
+//            Note note = adapter.getItem(i);
+//            String content = note.getNoteContent();
+//
+//            // TODO: This currently looks for exact string matches. It should split the query into words and search for each one
+//            if (content.contains(query)) {
+//                filteredAdapter.add(note);
+//            }
+//        }
         // TODO: The found notes should be ordered by relevance. If a note's content matches the search string perfectly then use found.add(0, note) to add it to the top (index zero)
         listView.setAdapter(filteredAdapter);
     }
